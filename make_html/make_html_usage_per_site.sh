19ef590847c918748ec247a4852c3239ee1a4ffd
Antonio Pérez-Calero Yzquierdo <aperez@pic.es>
2016-10-20 19:39:21 +0200

    updating html scripts

---
+++ make_html/make_html_usage_per_site.sh
@@ -0,0 +1,94 @@
+#Interval to plot in hours
+int=$1
+mysite=$2
+let n_lines=6*$int
+if [[ $int -gt "168" ]]; then  #put plots longer than one week at another location
+	long="long"
+else
+	long=""
+fi
+
+OUT="/crabprod/CSstoragePath/aperez/T2s/"$long"multicore_usage_"$mysite"_"$int"h.html"
+echo '<html>
+<head>
+<title>CMS multicore pilots core usage monitor</title>
+<!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">'>$OUT
+
+echo "google.load('visualization', '1', {packages: ['corechart', 'line']});
+google.setOnLoadCallback(drawChart);
+
+function drawChart() {">>$OUT
+
+for site in $(echo $mysite); do
+	#echo $site
+	echo "var data_$site = new google.visualization.DataTable();	
+	data_$site.addColumn('datetime', 'Date');
+      	data_$site.addColumn('number', 'Busy cores'); 
+	data_$site.addColumn('number', 'Idle cores');
+
+	data_$site.addRows([">>$OUT
+	tail -n $n_lines /home/aperez/out/count_$site >/home/aperez/status/input_file_use_$site$int
+	while read -r line; do
+		time=$(echo $line |awk '{print $1}')
+		let timemil=1000*$time
+		content=$(echo $line |awk '{print $5", "$6}')
+		echo "[new Date($timemil), $content], " >>$OUT
+	done </home/aperez/status/input_file_use_$site$int
+        list=$(python /home/aperez/get_averages.py /home/aperez/status/input_file_use_$site$int)
+        list_2=$(echo $list |awk -F"] " '{print $4"]", $5"]"}')
+        declare "stats_$site=$(echo $list_2)"
+	rm /home/aperez/status/input_file_use_$site$int
+
+	echo "      ]);
+
+        var options_$site = {
+                title: '$site',
+                isStacked: 'true',
+		explorer: {},
+                'height':500,
+		colors: ['#0040FF', '#FF0000'],
+                hAxis: {title: 'Time'},
+                vAxis: {title: 'Number of cores'}
+        };
+
+      	var chart_$site = new google.visualization.AreaChart(document.getElementById('chart_div_$site'));
+      	chart_$site.draw(data_$site, options_$site);">>$OUT
+done
+
+echo '
+    }
+
+    </script>
+<style>
+p {text-align: center;
+   font-family: verdana;
+        }
+</style>
+</head>
+
+<body>
+    <div id="header">
+        <h2>MULTICORE PILOT USAGE OF CPU CORES AT CMS '$mysite' site for the last '$int' hours<br>
+        <a href="http://submit-3.t2.ucsd.edu/CSstoragePath/aperez/T2s/multicore_occupancy_t2s_'$int'h.html">(OCCUPANCY)</a>
+        <a href="http://submit-3.t2.ucsd.edu/CSstoragePath/aperez/T2s/multicore_factory_t2s_'$int'h.html">(FACTORY STATUS)</a>
+        <a href="http://submit-3.t2.ucsd.edu/CSstoragePath/aperez/T2s/multicore_frontend_t2s_'$int'h.html">(FRONT-END)</a>
+        </h2>
+
+	</h2>
+    </div>
+<a href="http://submit-3.t2.ucsd.edu/CSstoragePath/aperez/T2s/multicore_usage_t2s_24h.html">24h</a>
+<a href="http://submit-3.t2.ucsd.edu/CSstoragePath/aperez/T2s/multicore_usage_t2s_168h.html">1week</a>
+<a href="http://submit-3.t2.ucsd.edu/CSstoragePath/aperez/T2s/longmulticore_usage_t2s_720h.html">1month</a>
+<br>
+ <!--Div to hold the charts-->'>>$OUT
+
+for site in $(echo $mysite); do
+        var="stats_$site"
+        echo ' <div id="chart_div_'$site'"></div><p>'$(echo "[avg, min, max]: " "${!var}")'</p><br><br>'	
+done>>$OUT
+
+echo "
+</body>
+</html>" >>$OUT
